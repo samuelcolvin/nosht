1d285148414f2177d362df7e7f90287186a82937
Samuel Colvin <s@muelcolvin.com>
2018-08-09 10:34:24 +0100

    fix unsub

---
+++ js/src/Unsubscribe.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import WithContext from './utils/context'
+
+export const UnsubscribeValid = WithContext(({ctx}) => (
+  <div>
+    <h1>Thank you for Unsubscribing</h1>
+    <p>You will not receive any more emails {ctx.company.company.name}.</p>
+  </div>
+))
+
+export const UnsubscribeInvalid = () => (
+  <div>
+    <h1>Invalid Unsubscrible link</h1>
+    <p>The link you followed was invalid.</p>
+  </div>
+)
