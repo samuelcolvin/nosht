1cfb6fb6d70a589b8fb5c2737d576cdcec0cd406
Samuel Colvin <s@muelcolvin.com>
2018-11-28 12:12:09 +0000

    tweak recaptcha

--- js/src/general/Recaptcha.js
+++ js/src/general/Recaptcha.js
@@ -1,29 +1,39 @@
 import React from 'react'
 import {load_script} from '../utils'
 
+const element_id = 'google-recaptcha'
+const load_callback = 'google_recaptcha_load_callback'
+const click_callback = 'google_recaptcha_click_callback'
+
+function grecaptcha_render () {
+  window.grecaptcha.render(element_id, {sitekey: process.env.REACT_APP_RECAPTCHA_KEY, callback: click_callback})
+}
+
 export default class Recaptcha extends React.Component {
   static reset () {
-    window._grecaptcha_load_callback()
+    try {
+      grecaptcha_render()
+    } catch (error) {
+      // already rendered is ok, ignore
+      if (error.message !== 'reCAPTCHA has already been rendered in this element') {
+        throw error
+      }
+    }
     window.grecaptcha.reset()
   }
 
   componentDidMount () {
-    window._grecaptcha_click_callback = t => this.props.callback(t)
-    if (window._grecaptcha_load_callback) {
+    window[click_callback] = t => this.props.callback(t)
+    if (window[load_callback]) {
       // recaptcha has already been loaded
       Recaptcha.reset()
     } else {
-      window._grecaptcha_load_callback = () => {
-        window.grecaptcha.render('google-recaptcha', {
-          sitekey: process.env.REACT_APP_RECAPTCHA_KEY,
-          callback: '_grecaptcha_click_callback'
-        })
-      }
-      load_script('https://www.google.com/recaptcha/api.js?onload=_grecaptcha_load_callback&render=explicit')
+      window[load_callback] = grecaptcha_render
+      load_script(`https://www.google.com/recaptcha/api.js?onload=${load_callback}&render=explicit`)
     }
   }
 
   render () {
-    return <div id="google-recaptcha"/>
+    return <div id={element_id}/>
   }
 }
