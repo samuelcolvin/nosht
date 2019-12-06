a0f76ceafd411618a1c653aecbfbbcdf3d7d8701
Samuel Colvin <s@muelcolvin.com>
2019-12-06 15:46:30 +0000

    wider dashboard

--- js/src/Dashboard.js
+++ js/src/Dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {Route, Switch, Link} from 'react-router-dom'
-import {Row, Col} from 'reactstrap'
+import {Nav, NavItem, NavLink} from 'reactstrap'
 import {NotFound} from './general/Errors'
 import WithContext from './utils/context'
 import {as_title} from './utils'
@@ -20,9 +20,13 @@ const details_uri = page => page.details_uri || `/dashboard/${page.name}/:id/`
 const MenuItem = ({page, location}) => {
   const uri = list_uri(page)
   const active = location.pathname.startsWith(uri) ? ' active' : ''
-  return <Link to={uri} className={'list-group-item list-group-item-action' + active}>
-    {page.title || as_title(page.name)}
-  </Link>
+  return (
+    <NavItem>
+      <NavItem tag={Link} to={uri} className={'nav-link' + active}>
+        {page.title || as_title(page.name)}
+      </NavItem>
+    </NavItem>
+  )
 }
 
 class Dashboard extends React.Component {
@@ -49,34 +53,30 @@ class Dashboard extends React.Component {
       {name: 'account', title: 'Account', details_comp: Account, details_uri: '/dashboard/account/'}
     )
     return (
-      <Row>
-        <Col md="3">
-        <div className="list-group mb-2">
+      <div>
+        <Nav pills className="mb-2">
           {pages.map(p => (
             <MenuItem key={p.name} page={p} location={this.props.location}/>
           ))}
-        </div>
-        </Col>
-        <Col md="9">
-          <Switch>
-            {pages.filter(p => p.list_comp).map(p => (
-              <Route key={p.name} exact path={list_match_uri(p)} render={props => {
-                const Comp = p.list_comp
-                return <Comp ctx={this.props.ctx} {...props} page={p}/>
-              }}/>
-            ))}
-            {pages.filter(p => p.details_comp).map(p => (
-              <Route key={p.name + '-details'} path={details_uri(p)} render={props => {
-                const Comp = p.details_comp
-                return <Comp ctx={this.props.ctx} {...props} page={p}/>
-              }}/>
-            ))}
+        </Nav>
+        <hr className="full-width"/>
+        <Switch>
+          {pages.filter(p => p.list_comp).map(p => (
+            <Route key={p.name} exact path={list_match_uri(p)} render={props => {
+              const Comp = p.list_comp
+              return <Comp ctx={this.props.ctx} {...props} page={p}/>
+            }}/>
+          ))}
+          {pages.filter(p => p.details_comp).map(p => (
+            <Route key={p.name + '-details'} path={details_uri(p)} render={props => {
+              const Comp = p.details_comp
+              return <Comp ctx={this.props.ctx} {...props} page={p}/>
+            }}/>
+          ))}
 
-            <Route component={NotFound}/>
-          </Switch>
-
-        </Col>
-      </Row>
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
     )
   }
 }
